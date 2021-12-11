6b49b70a9281ef212d2e195149bbd231e6b225f2
Steph Heins <stephanie_heins@yahoo.com>
2021-12-10 21:29:12 -0800

    added login & signup buttons to landingpage

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Styles/Landing.css'
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Button } from 'react-bootstrap';
 
 const Landing = () => {
     return (
@@ -8,7 +8,27 @@ const Landing = () => {
             <div className="main">
                 <Container>
                     <Row>
-                        Landing test
+                        <div>
+                            <h1>
+                                Welcome to NoteSafe
+                            </h1>
+                            <h3>
+                                A place to keep your notes 
+                                safe, organized, and accessible
+                            </h3>
+                        </div>
+                        <div className="my-3">
+                            <a href="/login" >
+                                <Button className="mx-3 btn-lg">
+                                    Login
+                                </Button>
+                            </a>
+                            <a href="/signup" >
+                                <Button className="mx-3 btn-lg">
+                                    Signup
+                                </Button>
+                            </a>
+                        </div>
                     </Row>
                 </Container>
             </div>
