bf647b85988f4a4eded1322a5f240e02ee3e33da
Steph Heins <stephanie_heins@yahoo.com>
2021-12-10 23:42:14 -0800

    added new note button

--- client/src/pages/Notes.js
+++ client/src/pages/Notes.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import './Styles/Notes.css'
 import MainScreen from '../components/MainScreen'
+import { Link } from 'react-router-dom';
+import { Button, Row } from 'react-bootstrap';
 
 const Notes = () => {
     return (
         <MainScreen title="Welcome User">
-            My Notes
+            <Row className="text-center">
+                <Link to="/CreateNote">
+                    <Button className="">
+                        New Note
+                    </Button>
+                </Link>
+            </Row>
         </MainScreen>
     )
 }
