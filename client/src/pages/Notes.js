0f8e3cd2e20a31775f07afde2e74e9f7f08f0473
Steph Heins <stephanie_heins@yahoo.com>
2021-12-11 21:03:13 -0800

    Added card for note info to Notes.js

--- client/src/pages/Notes.js
+++ client/src/pages/Notes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Styles/Notes.css'
 import MainScreen from '../components/MainScreen'
 import { Link } from 'react-router-dom';
-import { Button, Row } from 'react-bootstrap';
+import { Button, Row, Card, Col } from 'react-bootstrap';
 
 const Notes = () => {
     return (
@@ -13,6 +13,27 @@ const Notes = () => {
                         New Note
                     </Button>
                 </Link>
+            </Row>
+            <Row>
+                <Card className="my-4">
+                    <Card.Header>
+                        <Row>
+                        <Col id="notetitle">
+                            Title
+                        </Col>
+                        <Col className="text-end mt-2">
+                            Category
+                        </Col>
+                        </Row>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                        Note content...
+                        </Card.Text>
+                        <Button variant="primary">Edit</Button>
+                        <Button variant="primary" className="mx-3">Delete</Button>
+                    </Card.Body>
+                </Card>
             </Row>
         </MainScreen>
     )
