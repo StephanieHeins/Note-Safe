c7b542e9a517ef75ebe586a22645794f89bcefb2
Steph Heins <stephanie_heins@yahoo.com>
2021-12-11 22:20:53 -0800

    Added a file of static data, mapped some of it to Notes.js note cards

--- client/src/pages/Notes.js
+++ client/src/pages/Notes.js
@@ -3,10 +3,12 @@ import './Styles/Notes.css'
 import MainScreen from '../components/MainScreen'
 import { Link } from 'react-router-dom';
 import { Button, Row, Card, Col } from 'react-bootstrap';
+import noteinfo, {} from '../data/noteinfo';
 
 const Notes = () => {
     return (
         <MainScreen title="Welcome User">
+
             <Row className="text-center">
                 <Link to="/CreateNote">
                     <Button className="">
@@ -14,36 +16,42 @@ const Notes = () => {
                     </Button>
                 </Link>
             </Row>
+
             <Row>
+            {noteinfo.map((note) => (   
+
                 <Card className="my-4">
                     <Card.Header>
                         <Row>
                         <Col id="notetitle">
-                            Title
+                            {note.title}
                         </Col>
                         <Col className="text-end mt-2">
-                            Category
+                            {note.category}
                         </Col>
                         </Row>
                     </Card.Header>
                     <Card.Body>
                         <Card.Text>
-                            Note content...
+                            {note.content}
                         </Card.Text>
                         <Row>
                             <Col>
-                                <Button variant="primary">Edit</Button>
-                                <Button variant="primary" className="mx-3">Delete</Button>
+                                <Button variant="primary" className="me-3 mt-1">Edit</Button>
+                                <Button variant="primary" className="mt-1">Delete</Button>
                             </Col>
                             <Col className="mt-3">
-                                <Card.Text className="text-end" id="notedate">
+                                <Card.Text className="text-end mt-1" id="notedate">
                                     Written on 12-12-2021
                                 </Card.Text>
                             </Col>
                         </Row>
                     </Card.Body>
                 </Card>
-            </Row>
+
+
+            ))}
+                </Row>
         </MainScreen>
     )
 }
