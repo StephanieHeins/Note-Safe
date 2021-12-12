14c6c36bbcb8928bc4e3cc9dec6eb0118c20a777
Steph Heins <stephanie_heins@yahoo.com>
2021-12-12 02:19:08 -0800

    added user exists/user add to usercontrollers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,34 @@
-const registerUser = async (req, res) => {
+const asyncHandler = require('express-async-handler');
+const User = require('../models/usermodel');
+
+const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password, pic } = req.body
 
-    res.json({
+    const userExists = await User.findOne({email}); 
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists under this email.');
+    }
+
+    const user = await User.create({
         name,
-        email
+        email,
+        password,
+        pic
     });
-};
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            pic: user.pic
+        })
+    } else {
+        res.status(400)
+        throw new Error('Error')
+    }
+
+});
 
 module.exports = {registerUser}
