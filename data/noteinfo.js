651c5aee18fa21ea4c771f64f1eb88909403613b
Steph Heins <stephanie_heins@yahoo.com>
2021-12-11 23:42:06 -0800

    copy/pasted static data to test with server -- added api app.get for noteinfo data and to find by id

---
+++ data/noteinfo.js
@@ -0,0 +1,30 @@
+const noteinfo = [
+    {
+      _id: "1",
+      title: "Groceries",
+      content:
+        "-Eggs -Cream -Bread -Pepper",
+      category: "Errands",
+    },
+    {
+      _id: "2",
+      title: "Pet Store",
+      content: "-Dog Food -New Collar -Treats -Cat food -Litter",
+      category: "Errands",
+    },
+    {
+      _id: "3",
+      title: "Home",
+      content: "-Wrap christmas presents -Call mom -Feed cat -Check Mail",
+      category: "To-Do",
+    },
+    {
+      _id: 4,
+      title: "Homework",
+      content:
+        "Homework 5: Create a React App -- Due Nov. 13",
+      category: "School",
+    },
+  ];
+  
+  module.exports = noteinfo;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const noteinfo = require("./data/noteinfo")
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("API is Running"));
+
+app.get("/api/noteinfo", (req, res) => {
+    res.json(noteinfo)
+})
+
+app.get("/api/noteinfo/:id", (req, res) => {
+    const note = noteinfo.find((n)=>n._id===req.params.id);
+    res.send(note);
+})
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
