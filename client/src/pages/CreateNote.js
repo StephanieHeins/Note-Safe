e2941fdd59f07c8eed58d378049b3a98c6e2b5d7
Steph Heins <stephanie_heins@yahoo.com>
2022-01-07 17:46:49 -0800

    Added the functionality for creating a new note, appears properly in browser and in db

--- client/src/pages/CreateNote.js
+++ client/src/pages/CreateNote.js
@@ -1,14 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import MainScreen from "../components/MainScreen";
 import Loading from "../components/Loading";
-import { Link, useNavigate } from 'react-router-dom';
-import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import ErrorMessage from '../components/ErrorMessage';
+import { useNavigate } from 'react-router-dom';
+import { Container, Form, Button, Row, Col, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { createNoteAction } from "../actions/noteActions";
-import ErrorMessage from '../components/ErrorMessage';
 import reactMarkdown from 'react-markdown';
 
 const CreateNote = () => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [category, setCategory] = useState("");
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const noteCreate = useSelector((state) => state.noteCreate);
+    const { loading, error, note } = noteCreate;
+
+    console.log(note);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (!title || !content || !category) return;
+        dispatch(createNoteAction(title, content, category));
+
+        navigate('/notes');
+        window.location.reload();
+    };
+
     return (
         <div>
             <MainScreen title="New Note">
@@ -20,15 +41,16 @@ const CreateNote = () => {
                 <Col></Col>
                 <Col xs={6}>
 
-                    <Form>
+                    <Form onSubmit={submitHandler}>
+                        {error && <ErrorMessage variant="primary">{error}</ErrorMessage>}
 
                     <Form.Group className="mb-3" controlId="title">
                         <Form.Label className="formlabel">Title</Form.Label>
                         <Form.Control 
-                            type="input" 
+                            type="text" 
                             placeholder="Grocery List"
-                            // value={title}
-                            // onChange={(e) => setName(e.target.value)} 
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)} 
                         />
                     </Form.Group>
 
@@ -36,23 +58,32 @@ const CreateNote = () => {
                         <Form.Label className="formlabel">Notes</Form.Label>
                         <Form.Control 
                             as="textarea"
-                            rows="5"
-                            // type="" 
+                            rows={5}
                             placeholder="Eggs, Cheese, Milk..."
-                            // value={content}
-                            // onChange={(e) => setEmail(e.target.value)}
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
                         />
                     </Form.Group>
+                    {content && (
+                        <Card>
+                            <Card.Header>Preview</Card.Header>
+                            <Card.Body>
+                                <reactMarkdown>{content}</reactMarkdown>
+                            </Card.Body>
+                        </Card>
+                    )}
 
                     <Form.Group className="mb-3" controlId="category">
                         <Form.Label className="formlabel">Category</Form.Label>
                         <Form.Control 
-                            type="input" 
+                            type="category" 
                             placeholder="Shopping" 
-                            // value={password}
-                            // onChange={(e) => setPassword(e.target.value)}
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
                         />
                     </Form.Group>
+
+                    {loading && <Loading />}
 
                     <Row className="text-center mb-3">
                         <Col>
